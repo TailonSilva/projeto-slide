e2e1393353a04bc3a76794a1402d26b497216e34
Tailon Silva <tailontegner@live.com>
2023-02-27 22:41:59 -0300

    atualização dos valores de distancias

--- js/slide.js
+++ js/slide.js
@@ -2,22 +2,41 @@ export default class Slide {
   constructor(slider, slide) {
     this.slider = document.querySelector(slider);
     this.slide = document.querySelector(slide);
+    this.distancias = {
+      posicaoFinal: 0,
+      posicaoX: 0,
+      movimento: 0,
+      posicaoMovimento: 0,
+    };
+  }
+
+  movimenta(distanciaX) {
+    this.distancias.posicaoMovimento = distanciaX;
+    this.slide.style.transform = `translate3D(${distanciaX}px,0,0)`;
+  }
+
+  atualizaPosicao(posicaoX) {
+    this.distancias.movimento = (this.distancias.posicaoX - posicaoX) * 1.6;
+    return this.distancias.posicaoFinal - this.distancias.movimento;
   }
 
   // CONTROLE DOS EVENTOS
   quandoClicar(event) {
     event.preventDefault();
-    console.log('clicou');
+    this.distancias.posicaoX = event.clientX;
+    console.log(this.distancias.posicaoX);
     this.slider.addEventListener('mousemove', this.quandoMover);
   }
 
   quandoMover(event) {
-    console.log('moveu');
+    const posicaoFinal = this.atualizaPosicao(event.clientX);
+    this.movimenta(posicaoFinal);
   }
 
   quandoSoltar() {
-    console.log('soltou');
     this.slider.removeEventListener('mousemove', this.quandoMover);
+    this.distancias.posicaoFinal = this.distancias.posicaoMovimento;
+    console.log(this.distancias.posicaoFinal);
   }
 
   // ADICIONANDO OS EVENTOS
